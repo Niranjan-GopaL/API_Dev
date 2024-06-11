f640044267549b3ba7ea45d795410c9d6e1718bb
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2024-06-12 00:36:07 +0530

    finally I RETRIEVED ALL Y DATA !!!!!!!!!!!!!!!!!

--- app/connection_with_postgres.py
+++ app/connection_with_postgres.py
@@ -2,14 +2,21 @@
 from psycopg2.extras import RealDictCursor
 import psycopg2
 
+
 # Connect to your postgres DB
-conn = psycopg2.connect(host="localhost", database="Learning-API-Dev", user="postgres", password="123", cursor_factory=RealDictCursor)
+try :
+    conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+    # Execute a query
+    cur.execute('SELECT * FROM "Product" ; ')
 
-# Execute a query
-cur.execute('SELECT * FROM public."Product"')
+    # Retrieve query results
+    records = cur.fetchall()
+    print(records)
 
-# Retrieve query results
-records = cur.fetchall()
+    
+except Exception as error_var :
+    print("Failed to connect to DB ...")
+    print("Error :- \n ", error_var)
