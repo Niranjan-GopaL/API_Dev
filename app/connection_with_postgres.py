9cdcdad545fd6ffe2c910eaba36870d39da29c96
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2024-06-12 00:57:43 +0530

    while True until be connect to the db ; So even if DB failed to connect cuz of initilisation issues ; our code won't break ; it will keep on trying until it connects to the DB

--- app/connection_with_postgres.py
+++ app/connection_with_postgres.py
@@ -1,6 +1,7 @@
 # from psycopg2 import connect, cursor
 from psycopg2.extras import RealDictCursor
 import psycopg2
+from time import sleep
 
 
 # Connect to your postgres DB
@@ -10,13 +11,36 @@ try :
     cur = conn.cursor()
 
     # Execute a query
-    cur.execute('SELECT * FROM "Product" ; ')
+    cur.execute('SELECT * FROM public."Product" ; ')
 
     # Retrieve query results
     records = cur.fetchall()
     print(records)
 
-    
+
 except Exception as error_var :
     print("Failed to connect to DB ...")
     print("Error :- \n ", error_var)
+
+    
+    
+    
+# MUCH BETTER WAY TO DO THIS :-
+while True:
+    try :
+        # conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)
+
+        conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)  
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM public."Product" ; ')
+        records = cur.fetchall()
+        print(records)
+        break  # the only way for this loop to end is if IT SUCCESSFULLY CONNECTS to the db
+    
+    except Exception as error_var :
+        print("Failed to connect to DB ...")
+        print("Error :- \n ", error_var)
+        
+        sleep(2)  # 2 seconds()
+
+    
