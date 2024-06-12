fba676c137ad813d35f492ec3fd6601cb94671d9
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2024-06-12 17:08:56 +0530

    None is [] <--- False ; None == [] <--- False ; Remeber not <whatever> is the best  way to raise the execption whe n<whatever> is empty

--- scratch_pad.py
+++ scratch_pad.py
@@ -9,3 +9,9 @@ if 5 not in dictionary:
     dictionary[5] = "five"
 
 print(dictionary)
+
+# So if the result of a .fetchall() is empty, REMEMBER IT IS `[]`
+# if updated_post == []  <--- correct way
+# if not updated_post    <--- safest way
+print(  None is [] )
+print(  None == [] )
