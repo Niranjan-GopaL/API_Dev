3f385a0799acfaa976bc9ebcb1d20d971e218bce
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2024-06-12 08:53:18 +0530

    All you need is just 2 functions from pysopgy2, refactored import statements

--- app/connection_with_postgres.py
+++ app/connection_with_postgres.py
@@ -1,12 +1,11 @@
 # from psycopg2 import connect, cursor
-from psycopg2.extras import RealDictCursor
-import psycopg2
+from psycopg2.extras import RealDictCursor, connect 
 from time import sleep
 
 
 # Connect to your postgres DB
 try :
-    conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)
+    conn = connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)
     # Open a cursor to perform database operations
     cur = conn.cursor()
 
@@ -28,9 +27,16 @@ except Exception as error_var :
 # MUCH BETTER WAY TO DO THIS :-
 while True:
     try :
-        # conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)
+        # conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="wrong_password", cursor_factory=RealDictCursor)
 
-        conn = psycopg2.connect(host="localhost", database="Learning-API-dev", user="postgres", password="123", cursor_factory=RealDictCursor)  
+        # USE DOTENV and fill these data; you can have a; .env ; .env_for_development; .env_for_production; .env_for_testing
+        conn = connect(
+            host="localhost", 
+            database="Learning-API-dev", 
+            user="postgres", 
+            password="123", 
+            cursor_factory=RealDictCursor
+        )  
         cur = conn.cursor()
         cur.execute('SELECT * FROM public."Product" ; ')
         records = cur.fetchall()
