81b43e9c10f374c1168985a01a61bc4cc49af21b
Niranjan Gopal <Niranjan.Gopal@iiitb.ac.in>
2024-06-21 23:15:45 +0530

    Finished documenting the code in my own words ; really simple stuff

--- app/Oauth2.py
+++ app/Oauth2.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated, Union
-    
+
 import jwt
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -75,14 +75,15 @@ def authenticate_user(fake_db, username: str, password: str):
 
 
 
+# -----------------------------   2 IMPORTANT FUNCTIONS -----------------------------------------------------
 
-
-
+#           login_for_access_token() => gives the access token to a VALID USER
+#              create_access_token() => creates a jwt Token that will be used by login_for_access_token()
 
 # THIS IS THE MOST IMPORTANT PART OF CODE WE ARE FOCUSING ON
 
 # to create a token, we need => ` metadata + payload ` i.e DATA ` + signature ` i.e SIGNATURE
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str :
 
     # making a copy of the data ( so that original does not get tampered with )
     to_encode = data.copy()
@@ -101,8 +102,48 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     print(encoded_jwt)
     return encoded_jwt
 
+# VERY IMPORTANT PART
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    incorrect_credential_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    # get user from DB
+    user = authenticate_user(test_users_db, form_data.username, form_data.password)
+
+    # if user not found => raise exception
+    if not user:
+        raise incorrect_credential_exception
+
+    # create an ACCESS TOKEN ( a jwt token is created here ), if the VALID USER IS LOGGING IN !
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    
+    # return the token BACK TO THE CLIENT
+    return Token(access_token=access_token, token_type="bearer")
 
 
+
+
+
+
+
+
+# -------------------------------  NOW WE CAN PERFOEM ALL THE PATH OEPRATION THAT ONLY A VLID USER CAN DO ----------------------------------------
+
+# The API ROUTES that the valid users can access, begins here
+# FOR BOTH WE PASS THE DEPENDENCY => ` get_current_active_user ` 
+
+# each path operation has a dependency     => ` Depends(get_current_active_user) `
+# get_current_active_user has a dependency =>  `Depends(get_current_user)` 
+# { which is used to ensure that the user is authenticated and active }
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
@@ -110,6 +151,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -123,41 +165,20 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         raise credentials_exception
     return user
 
-
-
-
-
 async def get_current_active_user( current_user: Annotated[User, Depends(get_current_user)], ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
-@router.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-) -> Token:
-    user = authenticate_user(test_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
-
-
+# Profile page of the VALID USER
 @router.get("/users/me/", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
-):
+) -> User :
     return current_user
 
-
+# ITEMS of the VALID USER
 @router.get("/users/me/items/")
 async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)],
